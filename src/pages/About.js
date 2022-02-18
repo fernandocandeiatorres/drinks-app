cbd6f07b549083f2e20bcd37e1506db80366b857
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2022-02-17 22:37:25 -0300

    added files

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus neque
+        maiores, obcaecati, similique nihil eligendi amet accusamus illo vero
+        sed laudantium repellat dolorum officiis animi inventore distinctio
+        rerum eveniet nam. Repellat recusandae id voluptatibus, distinctio
+        voluptatum, neque dolor saepe iste rerum quae in debitis doloremque
+        provident vel aliquam perferendis nihil tempore corporis dolorum.
+        Incidunt ipsam dolorem quidem necessitatibus unde ratione!
+      </p>
+    </section>
+  );
+};
+
+export default About;
